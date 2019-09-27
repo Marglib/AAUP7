468ff34bf3e5163605f684a12f58cc5e36beac92
Alexander Bilgram <abkr16@student.aau.dk>
2019-09-27 10:19:47 +0200

    Fixed function in ReadResults

--- ReadResultFile.py
+++ ReadResultFile.py
@@ -12,23 +12,29 @@ def find_value(line, parameter, digits):
   	return line
 
 
-def generate_results(options, tripResultDir, tripFileDir):
+def generate_results(options, tripResultDir, tripFileDir, queueFileDir):
 	f = open(tripFileDir, "r+")
+	r = open(queueFileDir, "r+")
 
+	maxDuration = 0
+	maxTimeLoss = 0
+	maxWaitingTime = 0
+	maxQueueLength = 0
 	durationList = []
 	timeLossList = []
 	waitingTimeList = []
+	queueLengthList = []
+	queueLengthExpList = []
+
 
 	for line in f:
 		if "<tripinfo id" in line:
 		#Finds a value with 2 digits. 
+
 			durationList.append(find_value(line, "duration", 5))
 			timeLossList.append(find_value(line, "timeLoss", 5))
 			waitingTimeList.append(find_value(line, "waitingTime", 5))
 
-	def Average(lst):
-		lst = list(map(float, lst))
-		return sum(lst) / len(lst)
 
 	d = {'AverageDuration':[Average(durationList)],'AverageTimeLoss':[Average(timeLossList)],'AverageWaitingTime':[Average(waitingTimeList)]}
 	df = pd.DataFrame(d)
@@ -37,9 +43,14 @@ def generate_results(options, tripResultDir, tripFileDir):
 
 	f.close()
 
+def Average(lst):
+	lst = list(map(float, lst))
+	return sum(lst) / len(lst)
+
 def get_options():
     optParser = optparse.OptionParser()
     optParser.add_option("--tripinfofile", type="string", dest="tripinfofile", default="", help="--tripinforfile parses the trip info. They are in the results directory")
+    optParser.add_option("--queuefile", type="string", dest="queuefile", default="", help="--queuefile parses the queue info. They are in the results directory")
     options, args = optParser.parse_args()
     return options
                   
@@ -51,8 +62,8 @@ if __name__ == "__main__":
 	    sys.exit("A result file is neccesary")
 
     tripFileDir = "results/" + options.tripinfofile + ".xml"
+    queueFileDir = "results/" + options.queuefile + ".xml"
     tripResults = "results/Results_" + str(options.tripinfofile) + ".csv"
-    generate_results(options, tripResults, tripFileDir)
-
+    generate_results(options, tripResults, tripFileDir, queueFileDir)
 
 
