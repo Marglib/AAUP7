13976751620c598f31e771287e6418b91dbdbca8
PeterTaankvist <ptaank16@student.aau.dk>
2019-09-27 09:09:32 +0200

    Added queue output to runnerscript

--- Runnerscript.py
+++ Runnerscript.py
@@ -196,8 +196,10 @@ if __name__ == "__main__":
     # subprocess and then the python script connects and runs
     emissioninfofile = "results/emission" + str(options.expid) + ".xml"
     tripinfofile = "results/tripinfo" + str(options.expid) + ".xml"
+    queueinfofile = "results/queueinfo" + str(options.expid) + ".xml"
     sumoProcess = subprocess.Popen([sumoBinary, "-c", options.sumocfg, "--tripinfo-output", 
-                                   tripinfofile, "--emission-output", emissioninfofile, "--remote-port", str(options.port)], stdout=sys.stdout,
+                                   tripinfofile, "--emission-output", emissioninfofile, "--queue-output", queueinfofile, 
+                                   "--remote-port", str(options.port)], stdout=sys.stdout,
                                    stderr=sys.stderr)
     run(options)
     sumoProcess.wait()
